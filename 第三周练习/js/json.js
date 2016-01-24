f45a61becec932f114c5dc7bae0f22590fb28367
刘珂 <376060613@qq.com>
2016-01-24 21:48:58 +0800

    回到顶部，图片延迟加载

---
+++ 第三周练习/js/json.js
@@ -0,0 +1,102 @@
+var dataAry = [
+    {
+        logo: "http://img.ivsky.com/img/tupian/t/201404/03/wan-017.jpg",
+        title: "网络强国战略习近平与“十三五”十四大战略",
+        desc: "互联网是二十世纪人类最大的发明互联网是二十世纪."
+    },
+    {
+        logo: "http://img.ivsky.com/img/tupian/t/201404/03/wan-014.jpg",
+        title: "网络强国战略习近平与“十三五”十四大战略",
+        desc: "互联网是二十世纪人类最大的发明互联网是二十世纪."
+    },
+    {
+        logo: "http://img.ivsky.com/img/tupian/t/201404/03/wan-019.jpg",
+        title: "网络强国战略习近平与“十三五”十四大战略",
+        desc: "互联网是二十世纪人类最大的发明互联网是二十世纪."
+    },
+    {
+        logo: "http://img.ivsky.com/img/tupian/t/201404/03/wan-016.jpg",
+        title: "网络强国战略习近平与“十三五”十四大战略",
+        desc: "互联网是二十世纪人类最大的发明互联网是二十世纪."
+    },
+    {
+        logo: "http://img.ivsky.com/img/tupian/t/201404/03/wan-017.jpg",
+        title: "网络强国战略习近平与“十三五”十四大战略",
+        desc: "互联网是二十世纪人类最大的发明互联网是二十世纪."
+    },
+    {
+        logo: "http://img.ivsky.com/img/tupian/t/201404/03/wan-014.jpg",
+        title: "网络强国战略习近平与“十三五”十四大战略",
+        desc: "互联网是二十世纪人类最大的发明互联网是二十世纪."
+    },
+    {
+        logo: "http://img.ivsky.com/img/tupian/t/201404/03/wan-019.jpg",
+        title: "网络强国战略习近平与“十三五”十四大战略",
+        desc: "互联网是二十世纪人类最大的发明互联网是二十世纪."
+    },
+    {
+        logo: "http://img.ivsky.com/img/tupian/t/201404/03/wan-016.jpg",
+        title: "网络强国战略习近平与“十三五”十四大战略",
+        desc: "互联网是二十世纪人类最大的发明互联网是二十世纪."
+    },
+    {
+        logo: "http://img.ivsky.com/img/tupian/t/201404/03/wan-017.jpg",
+        title: "网络强国战略习近平与“十三五”十四大战略",
+        desc: "互联网是二十世纪人类最大的发明互联网是二十世纪."
+    },
+    {
+        logo: "http://img.ivsky.com/img/tupian/t/201404/03/wan-014.jpg",
+        title: "网络强国战略习近平与“十三五”十四大战略",
+        desc: "互联网是二十世纪人类最大的发明互联网是二十世纪."
+    },
+    {
+        logo: "http://img.ivsky.com/img/tupian/t/201404/03/wan-019.jpg",
+        title: "网络强国战略习近平与“十三五”十四大战略",
+        desc: "互联网是二十世纪人类最大的发明互联网是二十世纪."
+    },
+    {
+        logo: "http://img.ivsky.com/img/tupian/t/201404/03/wan-016.jpg",
+        title: "网络强国战略习近平与“十三五”十四大战略",
+        desc: "互联网是二十世纪人类最大的发明互联网是二十世纪."
+    },
+    {
+        logo: "http://img.ivsky.com/img/tupian/t/201404/03/wan-017.jpg",
+        title: "网络强国战略习近平与“十三五”十四大战略",
+        desc: "互联网是二十世纪人类最大的发明互联网是二十世纪."
+    },
+    {
+        logo: "http://img.ivsky.com/img/tupian/t/201404/03/wan-014.jpg",
+        title: "网络强国战略习近平与“十三五”十四大战略",
+        desc: "互联网是二十世纪人类最大的发明互联网是二十世纪."
+    },
+    {
+        logo: "http://img.ivsky.com/img/tupian/t/201404/03/wan-019.jpg",
+        title: "网络强国战略习近平与“十三五”十四大战略",
+        desc: "互联网是二十世纪人类最大的发明互联网是二十世纪."
+    },
+    {
+        logo: "http://img.ivsky.com/img/tupian/t/201404/03/wan-016.jpg",
+        title: "网络强国战略习近平与“十三五”十四大战略",
+        desc: "互联网是二十世纪人类最大的发明互联网是二十世纪."
+    },
+    {
+        logo: "http://img.ivsky.com/img/tupian/t/201404/03/wan-017.jpg",
+        title: "网络强国战略习近平与“十三五”十四大战略",
+        desc: "互联网是二十世纪人类最大的发明互联网是二十世纪."
+    },
+    {
+        logo: "http://img.ivsky.com/img/tupian/t/201404/03/wan-014.jpg",
+        title: "网络强国战略习近平与“十三五”十四大战略",
+        desc: "互联网是二十世纪人类最大的发明互联网是二十世纪."
+    },
+    {
+        logo: "http://img.ivsky.com/img/tupian/t/201404/03/wan-019.jpg",
+        title: "网络强国战略习近平与“十三五”十四大战略",
+        desc: "互联网是二十世纪人类最大的发明互联网是二十世纪."
+    },
+    {
+        logo: "http://img.ivsky.com/img/tupian/t/201404/03/wan-016.jpg",
+        title: "网络强国战略习近平与“十三五”十四大战略",
+        desc: "互联网是二十世纪人类最大的发明互联网是二十世纪."
+    }
+];
--- 第三周练习/js/utils.js
+++ 第三周练习/js/utils.js
@@ -1,13 +1,34 @@
 var utils = {
-    listToArray:function(likeAry){
-        var ary=[];
-        try{
-            ary=Array.prototype.slice.call(likeAry,0);
-        }catch(e){
-            for(var i=0;i<likeAry.length;i++){
-                ary[ary.length]=likeAry[i];
+    listToArray: function (likeAry) {
+        var ary = [];
+        try {
+            ary = Array.prototype.slice.call(likeAry, 0);
+        } catch (e) {
+            for (var i = 0; i < likeAry.length; i++) {
+                ary[ary.length] = likeAry[i];
             }
         }
         return ary;
+    },
+    toJSON: function (str) {
+        return "JSON" in window ? JSON.parse(str) : eval("(" + str + ")");
+    },
+    offset: function (curEle) {
+        var p = curEle.offsetParent, l = curEle.offsetLeft, t = curEle.offsetTop;
+        while (p) {
+            if (navigator.userAgent.indexOf("MSIE 8.0") < 0) {
+                l += p.clientLeft;
+                t += p.clientTop;
+            }
+            l += p.offsetLeft;
+            t += p.offsetTop;
+            p = p.offsetParent;
+        }
+        return {left: l, top: t};
+    },
+    getCss: function (curEle, attr) {
+        var reg = /^[+-]?(\d|[1-9]\d+)(\.\d+)?(px|em|rem|pt)$/, val = null;
+        val = "getComputedStyle" in window ? window.getComputedStyle(curEle, null)[attr] : curEle.currentStyle[attr];
+        return reg.test(val) ? parseFloat(val) : val;
     }
 };
